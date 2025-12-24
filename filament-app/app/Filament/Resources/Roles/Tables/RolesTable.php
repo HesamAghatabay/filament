1b3cfbebb845be34fdbf64b7badf71ffe949838e
Hesam Aghatabay <program.h.p.2023@gmail.com>
2025-12-24 15:02:11 +0330

    update Role..

---
+++ filament-app/app/Filament/Resources/Roles/Tables/RolesTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\Roles\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class RolesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->label('Updated')
+                    ->sortable()
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
